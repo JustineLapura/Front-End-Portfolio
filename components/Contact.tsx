ba0a5db73accf172d2f4e61d536a2da6828cdeae
Jahz10 <bullymedont.justify@gmail.com>
2023-07-02 12:19:20 +0800

    finished left section Contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,27 +1,61 @@
-import Image from 'next/image'
-import React from 'react'
-import profile from "/public/profile.jpg"
+import Image from "next/image";
+import React from "react";
+import profile from "/public/champion.jpg";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <div className='w-full lg:h-screen'>
+    <div className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Contact</p>
-        <h2 className='py-4 font-bold text-2xl'>Get in Touch</h2>
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Contact
+        </p>
+        <h2 className="py-4 font-bold text-2xl">Get in Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
-            {/* left */}
-            <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
-                <div className="lg:p-4 h-full">
-                    <div className="">
-                        <Image className='rounded-xl hover:scale-105 ease-in duration-300' src={profile} alt='Profile'/>
-                    </div>
+          {/* left */}
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            <div className="lg:p-4 h-full">
+              <div className="">
+                <Image
+                  className="rounded-xl hover:scale-105 ease-in duration-300"
+                  src={profile}
+                  alt="Profile"
+                />
+              </div>
+              <div className="">
+                <h2>Name here</h2>
+                <p>Front-End Developer</p>
+                <p>
+                  I am available for freelance or full-time position. Contact me
+                  and let's talk.
+                </p>
+              </div>
+              <div className="">
+                <p className="text-xl uppercase pt-8">Connect with me</p>
+                <div className="flex items-center justify-between py-4">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-500">
+                    <FaLinkedinIn />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-500">
+                    <FaGithub />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-500">
+                    <AiOutlineMail />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-500">
+                    <BsFillPersonLinesFill />
+                  </div>
                 </div>
+              </div>
             </div>
-            {/* right */}
+          </div>
+          {/* right */}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
