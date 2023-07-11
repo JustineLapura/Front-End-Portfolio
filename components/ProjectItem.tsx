461a9a1439d6f52b1e11fd1b7c543f74d96526be
Jahz10 <bullymedont.justify@gmail.com>
2023-07-11 23:09:18 +0800

    fixed the sizing of the images in projects component

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -10,8 +10,8 @@ type ProjectType = {
 
 const ProjectItem = ({title, img, url}: ProjectType ) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
-            <Image className='round-lg group-hover:opacity-10' src={img} alt="property"/>
+    <div className="relative flex items-center justify-center overflow-hidden h-[400px] w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+            <Image className='round-lg group-hover:opacity-10 h-full w-full rounded-lg' src={img} alt="property"/>
             <div className="hidden group-hover:block absolute top-[50-%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
               <h3 className='text-2xl text-white tracking-wider text-center font-bold'>{title}</h3>
               <p className='text-xl pb-4 pt-2 text-white text-center'>React JS</p>
