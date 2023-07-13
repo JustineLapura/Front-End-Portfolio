6b81410ea117a06adcd8b511a23ae291edad40d5
Jahz10 <bullymedont.justify@gmail.com>
2023-07-14 00:38:56 +0800

    implemented the dynamic rendering for schedule and services, booking functionalities and date, time validation

--- app/crypto/demo-1/appointments/page.tsx
+++ app/crypto/demo-1/appointments/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import React, { useContext } from "react";
 import { AppointmentContext } from "@/context/DentalClinicContext/AppointmentContext";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 import TimeScheduleContext from "@/context/DentalClinicContext/TimeScheduleContext";
 import ServicesContext from "@/context/DentalClinicContext/ServicesContext";
 
@@ -12,12 +12,25 @@ type AppointmentType = {
   time: string;
   service: string;
   status: string;
-}[];
+};
 
+
+type ScheduleType = {
+  day: string;
+  startTime: string;
+  endTime: string;
+};
+
+type ServiceType = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+};
 
 const page = () => {
-  const { schedule } = useContext(TimeScheduleContext)
-  const { services } = useContext(ServicesContext)
+  const { schedule } = useContext(TimeScheduleContext);
+  const { services } = useContext(ServicesContext);
   const {
     date,
     time,
@@ -32,40 +45,186 @@ const page = () => {
     setErrorMessage,
     appointments,
     setAppointments,
-    handleServiceChange
-  } = useContext(AppointmentContext)
+    handleServiceChange,
+  } = useContext(AppointmentContext);
 
-  // const handleBookAppointment = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   e.preventDefault();
-  
-  //   // Check if the selected date and time already exist in the appointments array
-  //   const isDuplicate = appointments.some((appointment:AppointmentType) => appointment.date === date && appointment.time === time);
-  
-  //   if (isDuplicate) {
-  //     setErrorMessage("This appointment date and time is already taken. Please choose a different date and time.");
-  //   } else if (date !== "" && time !== "" && service !== "" && !isInvalidDate && !isInvalidTime) {
-  //     const newAppointment = {
-  //       id: nanoid(),
-  //       name: `${selectedUser.firstName} ${selectedUser.lastName}`,
-  //       date,
-  //       time,
-  //       status: "Pending",
-  //       service
-  //     };
-  
-  //     // Update only the new appointment status to "Pending" and keep the existing appointments' statuses intact
-  //     const updatedAppointments = [newAppointment, ...appointments];
-  
-  //     setAppointments(updatedAppointments);
-  //     setErrorMessage(null);
-  //   } else {
-  //     setErrorMessage("Please fill the form correctly!");
-  //   }
-  // };
+  const handleBookAppointment = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
 
-  
+    // Check if the selected date and time already exist in the appointments array
+    const isDuplicate = appointments.some((appointment:AppointmentType) => appointment.date === date && appointment.time === time);
 
+    if (isDuplicate) {
+      setErrorMessage("This appointment date and time is already taken. Please choose a different date and time.");
+    } else if (date !== "" && time !== "" && service !== "" && !isInvalidDate && !isInvalidTime) {
+      const newAppointment = {
+        id: nanoid(),
+        name: "User",
+        date,
+        time,
+        status: "Pending",
+        service
+      };
 
+      // Update only the new appointment status to "Pending" and keep the existing appointments' statuses intact
+      const updatedAppointments = [newAppointment, ...appointments];
+
+      setAppointments(updatedAppointments);
+      setErrorMessage(null);
+    } else {
+      setErrorMessage("Please fill the form correctly!");
+    }
+  };
+
+  const sunday = schedule.find(
+    (sched: ScheduleType) => sched.day.toLowerCase() === "sunday"
+  ); // Find the schedule for Sunday
+  const monday = schedule.find(
+    (sched: ScheduleType) => sched.day.toLowerCase() === "monday"
+  ); // Find the schedule for Monday
+  const tuesday = schedule.find(
+    (sched: ScheduleType) => sched.day.toLowerCase() === "tuesday"
+  ); // Find the schedule for Tuesday
+  const wednesday = schedule.find(
+    (sched: ScheduleType) => sched.day.toLowerCase() === "wednesday"
+  ); // Find the schedule for Wednesday
+  const thursday = schedule.find(
+    (sched: ScheduleType) => sched.day.toLowerCase() === "thursday"
+  ); // Find the schedule for Thursday
+  const friday = schedule.find(
+    (sched: ScheduleType) => sched.day.toLowerCase() === "friday"
+  ); // Find the schedule for Friday
+  const saturday = schedule.find(
+    (sched: ScheduleType) => sched.day.toLowerCase() === "saturday"
+  ); // Find the schedule for Saturday
+
+  const isSunday = (date: string) => {
+    const day = new Date(date).getDay();
+    return day === 0; // 0 corresponds to Sunday
+  };
+
+  const isMonday = (date: string) => {
+    const day = new Date(date).getDay();
+    return day === 1; // 1 corresponds to Monday
+  };
+
+  const isTuesday = (date: string) => {
+    const day = new Date(date).getDay();
+    return day === 2; // 2 corresponds to Tuesday
+  };
+
+  const isWednesday = (date: string) => {
+    const day = new Date(date).getDay();
+    return day === 3; // 3 corresponds to Wednesday
+  };
+
+  const isThursday = (date: string) => {
+    const day = new Date(date).getDay();
+    return day === 4; // 4 corresponds to Thursday
+  };
+
+  const isFriday = (date: string) => {
+    const day = new Date(date).getDay();
+    return day === 5; // 5 corresponds to Friday
+  };
+
+  const isSaturday = (date: string) => {
+    const day = new Date(date).getDay();
+    return day === 6; // 6 corresponds to Saturday
+  };
+
+  const validateDate = (date: string) => {
+    const currentDate = new Date().toISOString().split("T")[0]; // Get the current date
+    const isSundayValid = !sunday || !sunday.startTime || !sunday.endTime; // Check if Sunday schedule is valid
+    const isMondayValid = !monday || !monday.startTime || !monday.endTime; // Check if Monday schedule is valid
+    const isTuesdayValid = !tuesday || !tuesday.startTime || !tuesday.endTime; // Check if Tuesday schedule is valid
+    const isWednesdayValid =
+      !wednesday || !wednesday.startTime || !wednesday.endTime; // Check if Wednesday schedule is valid
+    const isThursdayValid =
+      !thursday || !thursday.startTime || !thursday.endTime; // Check if Thursday schedule is valid
+    const isFridayValid = !friday || !friday.startTime || !friday.endTime; // Check if Friday schedule is valid
+    const isSaturdayValid =
+      !saturday || !saturday.startTime || !saturday.endTime; // Check if Saturday schedule is valid
+    return (
+      date < currentDate ||
+      (isSunday(date) && isSundayValid) ||
+      (isMonday(date) && isMondayValid) ||
+      (isTuesday(date) && isTuesdayValid) ||
+      (isWednesday(date) && isWednesdayValid) ||
+      (isThursday(date) && isThursdayValid) ||
+      (isFriday(date) && isFridayValid) ||
+      (isSaturday(date) && isSaturdayValid)
+    );
+  };
+
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedDate = e.target.value;
+    if (validateDate(selectedDate)) {
+      // Handle invalid date
+      setIsInvalidDate(true);
+      setIsInvalidTime(false);
+      setTime("");
+    } else {
+      setDate(selectedDate);
+      setIsInvalidDate(false);
+      setIsInvalidTime(false);
+      setTime("");
+    }
+  };
+
+  const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedTime = e.target.value;
+    const isValidTime = validateTime(selectedTime, date);
+
+    setTime(selectedTime);
+    setIsInvalidTime(!isValidTime);
+  };
+
+  const validateTime = (timeString: string, date: string) => {
+    const selectedTime = new Date(`2000-01-01T${timeString}`);
+    const day = new Date(date).getDay();
+
+    let scheduleForDay;
+    switch (day) {
+      case 0:
+        scheduleForDay = sunday;
+        break;
+      case 1:
+        scheduleForDay = monday;
+        break;
+      case 2:
+        scheduleForDay = tuesday;
+        break;
+      case 3:
+        scheduleForDay = wednesday;
+        break;
+      case 4:
+        scheduleForDay = thursday;
+        break;
+      case 5:
+        scheduleForDay = friday;
+        break;
+      case 6:
+        scheduleForDay = saturday;
+        break;
+      default:
+        return false;
+    }
+
+    if (
+      !scheduleForDay ||
+      !scheduleForDay.startTime ||
+      !scheduleForDay.endTime
+    ) {
+      // No schedule available for the selected day
+      return false;
+    }
+
+    const startTime = new Date(`2000-01-01T${scheduleForDay.startTime}`);
+    const endTime = new Date(`2000-01-01T${scheduleForDay.endTime}`);
+
+    return selectedTime >= startTime && selectedTime <= endTime;
+  };
 
   return (
     <div>
@@ -81,34 +240,48 @@ const page = () => {
                 </tr>
               </thead>
               <tbody className="text-center">
-                <tr className="border">
-                  <td> Monday </td>
-                  <td> 8:00 am - 5:00 pm </td>
-                </tr>
-                <tr className="border">
-                  <td>Tuesday</td>
-                  <td>Closed</td>
-                </tr>
-                <tr className="border">
-                  <td> Monday </td>
-                  <td> 8:00 am - 5:00 pm </td>
-                </tr>
-                <tr className="border">
-                  <td> Monday </td>
-                  <td> 8:00 am - 5:00 pm </td>
-                </tr>
-                <tr className="border">
-                  <td> Monday </td>
-                  <td> 8:00 am - 5:00 pm </td>
-                </tr>
-                <tr className="border">
-                  <td> Monday </td>
-                  <td> 8:00 am - 5:00 pm </td>
-                </tr>
-                <tr className="border">
-                  <td> Monday </td>
-                  <td> 8:00 am - 5:00 pm </td>
-                </tr>
+                {[
+                  "Monday",
+                  "Tuesday",
+                  "Wednesday",
+                  "Thursday",
+                  "Friday",
+                  "Saturday",
+                  "Sunday",
+                ].map((day) => {
+                  const daySchedule = schedule.find(
+                    (item: ScheduleType) => item.day === day
+                  );
+                  const startTime = daySchedule ? daySchedule.startTime : "";
+                  const endTime = daySchedule ? daySchedule.endTime : "";
+
+                  if (startTime && endTime) {
+                    return (
+                      <tr key={day}>
+                        <td>{day}</td>
+                        <td>
+                          {new Date(
+                            `2000-01-01T${startTime}`
+                          ).toLocaleTimeString([], {
+                            hour: "numeric",
+                            minute: "2-digit",
+                          })}{" "}
+                          -{" "}
+                          {new Date(`2000-01-01T${endTime}`).toLocaleTimeString(
+                            [],
+                            { hour: "numeric", minute: "2-digit" }
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  }
+                  return (
+                    <tr key={day}>
+                      <td>{day}</td>
+                      <td>Closed</td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
@@ -129,12 +302,16 @@ const page = () => {
                 <label className="font-bold">Choose a preferred date:</label>
                 <br />
                 <input
-                  className="form-control mb-1 w-1/2 border border-gray-400 py-2 px-4 rounded-lg text-center"
+                  className="cursor-pointer mb-1 w-1/2 border border-gray-400 py-2 px-4 rounded-lg text-center"
                   type="date"
+                  value={date}
+                  onChange={handleDateChange}
                 />
-                {/* <div className="text-red-500">
+                {isInvalidDate && (
+                  <div className="text-red-500">
                     Please select a date within the available schedule.
-                  </div> */}
+                  </div>
+                )}
               </div>
               <div className="relative mb-3">
                 {/* <img
@@ -145,21 +322,35 @@ const page = () => {
                 <label className="font-bold">Choose a preferred time:</label>
                 <br />
                 <input
-                  className="form-control mb-1 w-1/2 border border-gray-400 py-2 px-4 rounded-lg text-center"
+                  className="cursor-pointer mb-1 w-1/2 border border-gray-400 py-2 px-4 rounded-lg text-center"
                   type="time"
+                  value={time}
+                  onChange={handleTimeChange}
                 />
-                {/* <div className="text-red-500">
+                {isInvalidTime && (
+                  <div className="text-red-500">
                     Please select a time within the available schedule.
-                  </div> */}
+                  </div>
+                )}
               </div>
               <div className="mb-3">
                 <label className="font-bold">
                   Choose your preferred service:
                 </label>
                 <br />
-                <select className="form-control mb-1 w-1/2 mx-auto border border-gray-400 py-2 px-4 rounded-lg text-center lg:text-sm">
+                <select
+                  className="cursor-pointer mb-1 w-1/2 mx-auto border border-gray-400 py-2 px-4 rounded-lg text-center lg:text-sm"
+                  value={service}
+                  onChange={handleServiceChange}
+                >
                   <option value="">Select a service</option>
-                  <option>Sample Service</option>
+                  {services.map((service: ServiceType) => {
+                    return (
+                      <option key={service.id} value={service.name}>
+                        {service.name}
+                      </option>
+                    );
+                  })}
                 </select>
               </div>
               {/* <h6 className="mx-auto mt-2 text-red-500">Error messge</h6> */}
@@ -175,7 +366,7 @@ const page = () => {
             </form>
           </div>
           <div className="md:col-span-1 w-full sm:w-[75%] lg:w-full max-h-[450px] m-auto ">
-          <h1 className="text-center font-bold">Services Offered:</h1>
+            <h1 className="text-center font-bold">Services Offered:</h1>
             <table className="w-full table-fixed md:text-lg p-8 lg:text-sm bg-white text-black border rounded-lg overflow-hidden shadow-lg">
               <thead className="bg-blue-900 text-white">
                 <tr>
@@ -184,19 +375,12 @@ const page = () => {
                 </tr>
               </thead>
               <tbody className="text-center">
-                <tr className="border">
-                  <td> Monday </td>
-                  <td> 8:00 am - 5:00 pm </td>
-                </tr>
-                <tr className="border">
-                  <td>Tuesday</td>
-                  <td>Closed</td>
-                </tr>
-                <tr className="border">
-                  <td> Monday </td>
-                  <td> 8:00 am - 5:00 pm </td>
-                </tr>
-                
+                {services.map((service: ServiceType) => (
+                  <tr key={service.id}>
+                    <td>{service.name}</td>
+                    <td>P{service.price.toFixed(2)}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
