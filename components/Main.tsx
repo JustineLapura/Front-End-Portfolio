ebb3575cba8369efe68b2a39d8a28c4136ca4db3
JustineLapura <justinelapura002@gmail.com>
2024-07-22 15:17:38 +0800

    chore: update social links to open new tab

--- components/Main.tsx
+++ components/Main.tsx
@@ -43,14 +43,17 @@ const Main = () => {
             I am an aspiring junior full-stack web developer with a passion for
             comprehensive web development. I possess a strong foundation in both
             front-end and back-end technologies, including HTML, CSS,
-            JavaScript, React.js, Next.js, Bootstrap, Tailwind CSS, and TypeScript. In
-            addition to my proficiency in the client-side development, I am
-            well-versed in server-side technologies such as Node.js and
-            Express.js. My expertise extends to database management,
+            JavaScript, React.js, Next.js, Bootstrap, Tailwind CSS, and
+            TypeScript. In addition to my proficiency in the client-side
+            development, I am well-versed in server-side technologies such as
+            Node.js and Express.js. My expertise extends to database management,
             particularly with MongoDB.
           </motion.p>
           <div className="flex items-center justify-between text-gray-700 w-[70%] max-w-[330px] m-auto py-4">
-            <Link href="https://www.linkedin.com/in/justinelapura/">
+            <Link
+              target="_blank"
+              href="https://www.linkedin.com/in/justinelapura/"
+            >
               <motion.div
                 className="rounded-full text-xl cursor-pointer hover:text-[#5651e5] hover:scale-110 ease-in duration-200"
                 initial={{ y: 500 }}
@@ -60,7 +63,10 @@ const Main = () => {
                 <FaLinkedinIn />
               </motion.div>
             </Link>
-            <Link href="https://github.com/JustineLapura?tab=repositories">
+            <Link
+              target="_blank"
+              href="https://github.com/JustineLapura?tab=repositories"
+            >
               <motion.div
                 className="rounded-full text-xl cursor-pointer hover:text-[#5651e5] hover:scale-110 ease-in duration-200"
                 initial={{ y: 500 }}
@@ -80,7 +86,10 @@ const Main = () => {
                 <AiOutlineMail />
               </motion.div>
             </Link>
-            <Link href="https://www.facebook.com/justine.lapura.5">
+            <Link
+              target="_blank"
+              href="https://www.facebook.com/justine.lapura.5"
+            >
               <motion.div
                 className="rounded-full text-xl cursor-pointer hover:text-[#5651e5] hover:scale-110 ease-in duration-200"
                 initial={{ y: 500 }}
